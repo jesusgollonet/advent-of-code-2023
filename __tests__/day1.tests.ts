727c5f85f56f1fabb399ba2479243dc8967345de
Jesus Gollonet <jesusgollonet@gmail.com>
2024-08-07 12:04:18 +0200

    Structure with src/ util/ and data/ directories

--- __tests__/day1.tests.ts
+++ __tests__/day1.tests.ts
@@ -1,3 +1,7 @@
-test("adds 1 + 2 to equal 3", () => {
-  expect(1 + 2).toBe(3);
+import { findFirstLastDigit } from "../src/day1";
+
+describe("findFirstLastDigit", () => {
+  it("should return 12", () => {
+    expect(findFirstLastDigit("1abc2")).toBe(12);
+  });
 });
