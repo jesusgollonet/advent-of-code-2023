b3870ab2fc0db55742d7668608c408f4b25a352c
Jesus Gollonet <jesusgollonet@gmail.com>
2024-08-09 10:57:26 +0200

    update day 2 Tests

--- __tests__/day2.tests.ts
+++ __tests__/day2.tests.ts
@@ -1,12 +1,14 @@
 import solution, { parseGameLine, isGamePossible } from "../src/day2";
 
 describe("parse game line", () => {
-  it("should return the right number of cubes", () => {
-    const line = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green";
-    const parsed = parseGameLine(line);
-    const blueCubes = parsed["cubes"].blue;
-    expect(parsed).toMatchObject({ gameId: "1" });
-    expect(blueCubes).toBe(9);
+  const line = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green";
+  const parsed = parseGameLine(line);
+  it("should return the right number of rounds", () => {
+    expect(parsed.rounds.length).toBe(3);
+  });
+  it("rounds should have the right number of cubes", () => {
+    expect(parsed.rounds[0].blue).toBe(3);
+    expect(parsed.rounds[0].red).toBe(4);
   });
 });
 
@@ -24,9 +26,23 @@ describe("is game possible", () => {
       isGamePossible(questionArrangement, impossibleTargetArrangement),
     ).toBe(false);
   });
+  it("should not error when a round doesn't have all colors", () => {
+    const impossibleTargetArrangement = { green: 26, blue: 11, red: 25 };
+    const smallerRound = { blue: 1 };
+    expect(isGamePossible(smallerRound, impossibleTargetArrangement)).toBe(
+      false,
+    );
+  });
 });
 describe("solution", () => {
-  it("should return the right solution", () => {
-    //expect(solution()).toBe(0);
+  const day2Input = [
+    "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+    "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+    "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+    "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+    "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
+  ];
+  it("should return the right sum of ids", () => {
+    expect(solution(day2Input, { red: 12, green: 13, blue: 14 })).toBe(8);
   });
 });
