37ad35cd8f7944e4efcb122f60b8f9be6444a06b
Jesus Gollonet <jesusgollonet@gmail.com>
2024-08-09 18:26:34 +0200

    Day 3 mock input solved

--- src/day3.ts
+++ src/day3.ts
@@ -1,9 +1,11 @@
+const isSymbol = (s: string): boolean => !s.match(/[a-zA-z0-9.]/);
 export function solutionPart1(schematic: string[]): number {
+  let numbersKept: string[] = [];
   for (let i = 0; i < schematic.length; i++) {
     const line = schematic[i];
     let inNumber = false;
-    let numberStringsInLine: string[] = [];
     let currentNumber = "";
+    let keepNumber = false;
     for (let j = 0; j < line.length; j++) {
       let currentChar = line[j];
       if (currentChar >= "0" && currentChar <= "9") {
@@ -11,20 +13,60 @@ export function solutionPart1(schematic: string[]): number {
           inNumber = true;
           // check pos -1 and current pos, including  1 up 1 down
           currentNumber = currentChar;
+          for (
+            let k = Math.max(i - 1, 0);
+            k <= Math.min(i + 1, schematic.length - 1);
+            k++
+          ) {
+            // prevous position if we're not at the start of the line
+            if (j > 0) {
+              if (isSymbol(schematic[k].charAt(j - 1))) {
+                keepNumber = true;
+              }
+            }
+            if (isSymbol(schematic[k].charAt(j))) {
+              keepNumber = true;
+            }
+          }
         } else {
           currentNumber = currentNumber.concat(currentChar);
+          for (
+            let k = Math.max(i - 1, 0);
+            k <= Math.min(i + 1, schematic.length - 1);
+            k++
+          ) {
+            if (isSymbol(schematic[k].charAt(j))) {
+              keepNumber = true;
+            }
+          }
         }
       } else {
         if (inNumber) {
-          numberStringsInLine.push(currentNumber);
-          currentNumber = "";
-          // this is the end of the number
-          inNumber = false;
+          if (j < line.length - 1) {
+            for (
+              let k = Math.max(i - 1, 0);
+              k <= Math.min(i + 1, schematic.length - 1);
+              k++
+            ) {
+              if (isSymbol(schematic[k].charAt(j))) {
+                keepNumber = true;
+              }
+            }
+          }
+
+          if (keepNumber) {
+            numbersKept.push(currentNumber);
+
+            currentNumber = "";
+            // this is the end of the number
+            inNumber = false;
+            keepNumber = false;
+          }
         }
       }
     }
-    console.log(line);
-    console.log(numberStringsInLine);
   }
-  return 4361;
+  return numbersKept
+    .map((n) => parseInt(n))
+    .reduce((prev, current) => prev + current, 0);
 }
